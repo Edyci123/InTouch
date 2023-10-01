0056bbd04ed9e509ba477c0b10590eaf0083d97b
Edi Bicu <bicuedici@gmail.com>
2023-10-01 12:14:39 +0300

    qr code scan

--- src/app/src/pages/QRActions/QRActions.tsx
+++ src/app/src/pages/QRActions/QRActions.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { BasePage } from "../../components/BasePage/BasePage";
 import QRCode from "react-qr-code";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
+import { IonButton } from "@ionic/react";
 
 export const QRActions: React.FC = () => {
+
+    const [scanned, setScanned] = useState('');
+
+    const scanQRCode = async () => {
+        const result = await BarcodeScanner.scan();
+        console.log(result);
+        setScanned(result.text);
+    }
+
     return (
         <BasePage
             title="QR"
             content={
                 <div className="ion-align-center">
                     <QRCode value="soemthing" />
+                    <IonButton onClick={() => scanQRCode()}>Scan</IonButton>
                 </div>
             }
         />
